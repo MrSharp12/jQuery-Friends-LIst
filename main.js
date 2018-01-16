1f8d2e225f1540e90fb5e34469704b5c597c3fbf
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-01-16 16:25:38 -0500

    added final files

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ $(document).ready(function(){
         let newElement = createFriend(newFriend);
         $('#friend-list').append(newElement);
         $('#input').val('');
-    }))
+    }));
+
+    $('#friend-list').on('click', 'li', function(e) {
+        $(this).remove();
+    })
 
     function createFriend(name) {
         return $(`<li>${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
